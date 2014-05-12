87b5f482cbf5698cbf130cdac1328cd3e7ccc67b
lancethepants <lancethepants@gmail.com>
2014-05-12 14:55:03 -0600

    Add lftp script and update README.

---
+++ lftp.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir ~/lftp && cd ~/lftp
+
+BASE=`pwd`
+SRC=$BASE/src
+WGET="wget --prefer-family=IPv4"
+DEST=$BASE/opt
+LDFLAGS="-L$DEST/lib -Wl,--gc-sections"
+CPPFLAGS="-I$DEST/include"
+CFLAGS="-mtune=mips32 -mips32 -ffunction-sections -fdata-sections"
+CXXFLAGS=$CFLAGS
+CONFIGURE="./configure --prefix=/opt --host=mipsel-linux"
+MAKE="make -j`nproc`"
+mkdir -p $SRC
+
+######## ####################################################################
+# ZLIB # ####################################################################
+######## ####################################################################
+
+mkdir $SRC/zlib && cd $SRC/zlib
+$WGET http://zlib.net/zlib-1.2.8.tar.gz
+tar zxvf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+CROSS_PREFIX=mipsel-linux- \
+./configure \
+--prefix=/opt
+
+$MAKE
+make install DESTDIR=$BASE
+
+########### #################################################################
+# OPENSSL # #################################################################
+########### #################################################################
+
+mkdir -p $SRC/openssl && cd $SRC/openssl
+$WGET http://www.openssl.org/source/openssl-1.0.1g.tar.gz
+tar zxvf openssl-1.0.1g.tar.gz
+cd openssl-1.0.1g
+
+cat << "EOF" > openssl.patch
+--- Configure_orig      2013-11-19 11:32:38.755265691 -0700
++++ Configure   2013-11-19 11:31:49.749650839 -0700
+@@ -402,6 +402,7 @@ my %table=(
+ "linux-alpha+bwx-gcc","gcc:-O3 -DL_ENDIAN -DTERMIO::-D_REENTRANT::-ldl:SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_RISC1 DES_UNROLL:${alpha_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+ "linux-alpha-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
+ "linux-alpha+bwx-ccc","ccc:-fast -readonly_strings -DL_ENDIAN -DTERMIO::-D_REENTRANT:::SIXTY_FOUR_BIT_LONG RC4_CHAR RC4_CHUNK DES_INT DES_PTR DES_RISC1 DES_UNROLL:${alpha_asm}",
++"linux-mipsel", "gcc:-DL_ENDIAN -DTERMIO -O3 -mtune=mips32 -mips32 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${mips32_asm}:o32:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+
+ # Android: linux-* but without -DTERMIO and pointers to headers and libs.
+ "android","gcc:-mandroid -I\$(ANDROID_DEV)/include -B\$(ANDROID_DEV)/lib -O3 -fomit-frame-pointer -Wall::-D_REENTRANT::-ldl:BN_LLONG RC4_CHAR RC4_CHUNK DES_INT DES_UNROLL BF_PTR:${no_asm}:dlfcn:linux-shared:-fPIC::.so.\$(SHLIB_MAJOR).\$(SHLIB_MINOR)",
+EOF
+
+patch < openssl.patch
+
+./Configure linux-mipsel \
+-ffunction-sections -fdata-sections -Wl,--gc-sections \
+--prefix=/opt shared zlib zlib-dynamic \
+--with-zlib-lib=$DEST/lib \
+--with-zlib-include=$DEST/include
+
+make CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib
+make install CC=mipsel-linux-gcc AR="mipsel-linux-ar r" RANLIB=mipsel-linux-ranlib INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
+
+########### #################################################################
+# NCURSES # #################################################################
+########### #################################################################
+
+mkdir $SRC/curses && cd $SRC/curses
+$WGET http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
+tar zxvf ncurses-5.9.tar.gz
+cd ncurses-5.9
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--enable-widec \
+--disable-database \
+--with-fallbacks=xterm \
+--enable-overwrite
+
+$MAKE
+make install DESTDIR=$BASE
+ln -s libncursesw.a $DEST/lib/libcurses.a
+
+############### #############################################################
+# LIBREADLINE # #############################################################
+############### #############################################################
+
+mkdir $SRC/libreadline && cd $SRC/libreadline
+$WGET ftp://ftp.cwru.edu/pub/bash/readline-6.2.tar.gz
+tar zxvf readline-6.2.tar.gz
+cd readline-6.2
+
+$WGET https://raw.github.com/lancethepants/tomatoware/master/patches/readline.patch
+patch < readline.patch
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--disable-shared
+
+$MAKE
+make install DESTDIR=$BASE
+
+######## ####################################################################
+# LFTP # ####################################################################
+######## ####################################################################
+
+mkdir -p $SRC/lftp && cd $SRC/lftp
+$WGET http://lftp.yar.ru/ftp/lftp-4.4.16.tar.gz
+tar zxvf lftp-4.4.16.tar.gz
+cd lftp-4.4.16
+
+LDFLAGS=$LDFLAGS \
+CPPFLAGS=$CPPFLAGS \
+CFLAGS=$CFLAGS \
+CXXFLAGS=$CXXFLAGS \
+$CONFIGURE \
+--with-openssl=$DEST
+
+
+$MAKE LIBS="-all-static -lssl -lcrypto -lz -lcurses -ldl -lsupc++ -lutil"
+make install DESTDIR=$BASE/lftp
